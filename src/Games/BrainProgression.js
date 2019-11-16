1256ddf06eae8570974cfbb3bade3c31a2d8ff99
Yura Chemeris <chimikjunior@gmail.com>
2019-11-16 22:49:26 +0200

    added new game brain-progression

---
+++ src/Games/BrainProgression.js
@@ -0,0 +1,43 @@
+import {
+  welcome, hello, makeUserAnswer, makeRandomNumber,
+} from '../index';
+
+const brainProgression = () => {
+  welcome();
+  console.log('What number is missing in the progression?');
+  const userName = hello();
+  console.log(`Hello, ${userName}!`);
+  let answerCounter = 0;
+  for (let i = 0; i < 3; i += 1) {
+    const makeRandomDisNumber = () => Math.ceil(Math.random() * 8) + 2;
+    const randomDisNum = makeRandomDisNumber();
+    const randomProgressNum = makeRandomNumber();
+    let progressNum = 1;
+    let disNum = 0;
+    let newStr = '';
+    for (let k = 2; k <= 10; k += 1) {
+      progressNum += randomProgressNum;
+      if (k === randomDisNum) {
+        newStr += '.. ';
+        disNum = progressNum;
+        continue;
+      }
+      newStr += `${progressNum} `;
+    }
+    console.log(`Question: ${newStr} ?`);
+    const answer = makeUserAnswer();
+    if (+answer === +disNum) {
+      console.log('Correct!');
+      answerCounter += 1;
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${disNum}'.
+    Let's try again, ${userName}!`);
+      break;
+    }
+  }
+  if (answerCounter === 3) {
+    console.log(`Congratulations, ${userName}!`);
+  }
+};
+
+export default brainProgression;
