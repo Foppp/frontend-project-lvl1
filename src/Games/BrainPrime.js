c11c5116045b3805b2c5e04eaf60920aaac7ef4e
Yura Chemeris <chimikjunior@gmail.com>
2019-12-09 16:42:04 +0200

    brain-prime modified

--- src/Games/BrainPrime.js
+++ src/Games/BrainPrime.js
@@ -5,7 +5,6 @@ import {
 const primeGameRule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const primeGenerateQuestion = () => {
   const num = Math.ceil(Math.random() * 100);
-  let rightAnswer = 'yes';
   const isPrimeCheck = (x) => {
     for (let k = 2; k < x; k += 1) {
       if (x % k === 0) {
@@ -13,11 +12,7 @@ const primeGenerateQuestion = () => {
       }
     } return true;
   };
-  if (isPrimeCheck(num) === false) {
-    rightAnswer = 'no';
-  } else {
-    rightAnswer = 'yes';
-  }
+  const rightAnswer = isPrimeCheck(num) === true ? 'yes' : 'no';
   return cons(num, rightAnswer);
 };
 const primeRunIt = () => makeGame(primeGameRule, primeGenerateQuestion);
