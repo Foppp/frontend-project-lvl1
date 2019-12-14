4d4ecd4c0c8b8aa3de4aa9b5bc1fbe4feedec47b
Yura Chemeris <chimikjunior@gmail.com>
2019-12-14 21:38:45 +0200

    brain calc calculating answer function modified

--- src/Games/brain_calc.js
+++ src/Games/brain_calc.js
@@ -4,30 +4,30 @@ import { makeGame, generateNumber } from '../index';
 
 const calcGameRule = 'What is the result of the expression?';
 const operators = ['+', '-', '*'];
+const answer = (generatedOperator, num1, num2) => {
+  let ans = 0;
+  switch (generatedOperator) {
+    case '+':
+      ans = num1 + num2;
+      break;
+    case '-':
+      ans = num1 - num2;
+      break;
+    case '*':
+      ans = num1 * num2;
+      break;
+    default:
+      return false;
+  }
+  return ans;
+};
 const calcGenerateQuestion = () => {
   const number1 = generateNumber(1, 30);
   const number2 = generateNumber(1, 30);
-  const randomOperator = generateNumber(0, operators.length);
+  const randomOperator = generateNumber(0, operators.length - 1);
   const makeOperator = operators[randomOperator];
   const isQuestion = `${number1} ${makeOperator} ${number2}`;
-  const answer = (generatedOperator) => {
-    let ans = 0;
-    switch (generatedOperator) {
-      case '+':
-        ans = number1 + number2;
-        break;
-      case '-':
-        ans = number1 - number2;
-        break;
-      case '*':
-        ans = number1 * number2;
-        break;
-      default:
-        return false;
-    }
-    return ans;
-  };
-  const rightAnswer = answer(makeOperator).toString();
+  const rightAnswer = answer(makeOperator, number1, number2).toString();
   return cons(isQuestion, rightAnswer);
 };
 const calcRunIt = () => makeGame(calcGameRule, calcGenerateQuestion);
