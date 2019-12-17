84976ba5128c65388134f9d11bbf099c7671c525
Yura Chemeris <chimikjunior@gmail.com>
2019-12-17 22:17:59 +0200

    isEven true or false modified

--- src/Games/even.js
+++ src/Games/even.js
@@ -3,11 +3,7 @@ import { cons } from '@hexlet/pairs';
 import { makeGame, generateNumber } from '../index';
 
 const evenGameRule = 'Answer "yes" if the number is even, otherwise answer "no".';
-const isEven = (a) => {
-  if (a % 2 === 0) {
-    return true;
-  } return false;
-};
+const isEven = (a) => a % 2 === 0;
 const evenGenerateQuestion = () => {
   const isQuestion = generateNumber(1, 100);
   const rightAnswer = isEven(isQuestion) ? 'yes' : 'no';
