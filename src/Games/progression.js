84f2396e377694fd8cdf779f93a6d267e2ef9c1c
Yura Chemeris <chimikjunior@gmail.com>
2019-12-24 16:31:28 +0200

    modifyed progression finding method

--- src/Games/progression.js
+++ src/Games/progression.js
@@ -6,24 +6,25 @@ import makeGame from '..';
 
 const gameRule = 'What number is missing in the progression?';
 const progressionLength = 10;
+const maxStartNumber = 30;
+const maxDiffNumber = 20;
+const maxMissingIndexNumber = 8;
 const generateData = () => {
-  const missingNumberIndex = generateNumber(2, 10);
-  const progressionStep = generateNumber(1, 30);
-  let progressionNumber = 1;
-  let missingNumber;
-  let newStr = '';
-  for (let k = 2; k <= progressionLength; k += 1) {
-    progressionNumber += progressionStep;
-    if (k === missingNumberIndex) {
-      newStr = `${newStr}.. `;
-      missingNumber = progressionNumber;
+  let startMember = generateNumber(1, maxStartNumber);
+  const diffMember = generateNumber(1, maxDiffNumber);
+  let missingIndexMember = generateNumber(0, maxMissingIndexNumber);
+  let str = '';
+  for (let i = 0; i < progressionLength; i += 1) {
+    startMember += diffMember;
+    if (i === missingIndexMember) {
+      str = `${str}.. `;
+      missingIndexMember = startMember;
     } else {
-      newStr = `${newStr}${progressionNumber} `;
+      str = `${str}${startMember} `;
     }
   }
-  const question = newStr;
-  const rightAnswer = missingNumber.toString();
-
+  const question = str;
+  const rightAnswer = missingIndexMember.toString();
   return cons(question, rightAnswer);
 };
 const runGame = () => makeGame(gameRule, generateData);
