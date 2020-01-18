53a7bddbb34e3bad58ed42a207e34d3d206ab5f9
Yura Chemeris <chimikjunior@gmail.com>
2020-01-18 12:06:06 +0200

    renamed main function name

--- src/Games/gcd.js
+++ src/Games/gcd.js
@@ -6,7 +6,7 @@ import makeGame from '..';
 
 const gameRule = 'Find the greatest common divisor of given numbers.';
 const maxRandomNum = 50;
-const findBiggerDenom = (num1, num2) => {
+const findGcd = (num1, num2) => {
   let biggerDenom;
   for (let i = 1; i < num1; i += 1) {
     if (num1 % i === 0 && num2 % i === 0) {
@@ -19,7 +19,7 @@ const generateData = () => {
   const number1 = generateNumber(1, maxRandomNum);
   const number2 = generateNumber(1, maxRandomNum);
   const question = `${number1} ${number2}`;
-  const rightAnswer = findBiggerDenom(number1, number2).toString();
+  const rightAnswer = findGcd(number1, number2).toString();
   return cons(question, rightAnswer);
 };
 const runGame = () => makeGame(gameRule, generateData);
